d7926146fa9fb431d6c4936a1878f210edf269a5
Joel Stein <js@funkturm.net>
2020-12-30 12:08:34 +0530

    garden: Shim missing libshowlogo symbol
    * Q: https://github.com/LineageOS/android_frameworks_native/blob/lineage-17.1/libs/gui/SurfaceComposerClient.cpp#L1243
      - IDA: `android::SurfaceComposerClient::Transaction::setDisplayProjection(android::sp<android::IBinder> const&, unsigned int, android::Rect const&, android::Rect const&)
    	_ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEEjRKNS_4RectES9_`
    * R: https://github.com/LineageOS/android_frameworks_native/blob/lineage-18.1/libs/gui/SurfaceComposerClient.cpp#L1527
      - IDA: `android::SurfaceComposerClient::Transaction::setDisplayProjection(android::sp<android::IBinder> const&, android::ui::Rotation, android::Rect const&, android::Rect const&)
    	_ZN7android21SurfaceComposerClient11Transaction20setDisplayProjectionERKNS_2spINS_7IBinderEEENS_2ui8RotationERKNS_4RectESB_`
    
    Co-authored-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: Iba80fb6dd6a79d6b363d8788628b3b9a75305466
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,3 +174,7 @@ BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_USES_RECOVERY_AS_BOOT := false
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Symbols
+TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
+
