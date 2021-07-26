430fa37c6b28ca1f4f7f537e52e532baac4deea9
Ash Blake <ash.blake@tutanota.com>
2021-07-26 20:17:53 +0200

    garden: dt2w: autodetect touchscreen event device
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -68,6 +68,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DT2W-Service-Garden
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/dt2w/dt2w_event:$(TARGET_COPY_OUT_SYSTEM)/bin/dt2w_event
+
 # Battery
 PRODUCT_PACKAGES += \
     BatteryHealthOverlay
